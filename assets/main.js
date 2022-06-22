5943728253e06eaf80737127447f869621b4e283
elvinarioba <88180411+elvinarioba@users.noreply.github.com>
2022-06-22 21:15:19 +0300

    First commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const myfunction = () => {
+	let userInput = document.querySelector("#text");
+	 const generatedElement = document.createElement('p');
+	 generatedElement.innerHTML = `Hello ${userInput.value}, welcome to the bootcamp`;
+	 document.body.appendChild(generatedElement);
+ }
